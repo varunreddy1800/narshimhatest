78fd61e8de2fe219e37716fbc930df10c85636a8
Nuvvala <narasimha.nuvvala@gmail.com>
2015-07-18 20:22:44 -0500

    Create Test Project

---
+++ Test/Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Nuvvala on 7/18/15.
+//  Copyright (c) 2015 Narasimha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
